a808fe8348a6f44f5882ffdf892feca4e111d26c
Jyothipeethani <jyothipeethani178@gmail.com>
2024-07-09 20:48:51 +0530

    feature202 changes by vivek

--- Application.java
+++ Application.java
@@ -16,5 +16,27 @@ class Test
        public static void main(String[] args)    
        { 
            System.out.println("Class File Structure"); 
+#feature202 changes by vivek for the Reports module in july2024
+public static void main(String[] args) {
+
+    char[] array = new char[100];
+    try {
+      // Creates a reader using the FileReader
+      FileReader input = new FileReader("input.txt");
+
+      // Reads characters
+      input.read(array);
+      System.out.println("Data in the file:");
+      System.out.println(array);
+
+      // Closes the reader
+      input.close();
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+    }
+  }
+}
+#end of the code feature202
        } 
 }  
